0a04ac86d0440a781053c63a305772235c1b3b55
Gustavo <gustavo.ccx@hotmail.com>
2025-02-07 17:29:49 -0300

    INT-2
    - Criado Tela de Login
    - Adicionado Rota de Login
    - Adicionado Angular Material

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,22 @@
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+  imports: [MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+
+export class LoginComponent {
+
+  hide = signal(true);
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
+}
