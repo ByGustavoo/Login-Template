1d2d59dcb037b49838ccb5c5ede8b1cd2405cbc3
Gustavo <gustavo.ccx@hotmail.com>
2025-02-10 18:27:41 -0300

    LTP-4
    - Criado validação do e-mail inserido
    - Valida se digitou um e-mail e se é válido

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -4,20 +4,42 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router'; 
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
+import {FormControl, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {merge} from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
-  imports: [MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule, RouterModule],
+  imports: [MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule, RouterModule, FormsModule, ReactiveFormsModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 
 export class LoginComponent {
+  readonly email = new FormControl('', [Validators.required, Validators.email]);
 
   hide = signal(true);
   clickEvent(event: MouseEvent) {
     this.hide.set(!this.hide());
     event.stopPropagation();
+  }
+
+  errorMessage = signal('');
+
+  constructor() {
+    merge(this.email.statusChanges, this.email.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateErrorMessage());
+  }
+
+  updateErrorMessage() {
+    if (this.email.hasError('required')) {
+      this.errorMessage.set('Por favor, digite um e-mail!');
+    } else if (this.email.hasError('email')) {
+      this.errorMessage.set('Por favor, digite um e-mail válido!');
+    } else {
+      this.errorMessage.set('');
+    }
   }
 }
