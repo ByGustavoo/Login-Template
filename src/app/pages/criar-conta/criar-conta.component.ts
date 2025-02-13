0a87ce39715f2e7e9d57836c12ab1b1de6225147
Gustavo <gustavo.ccx@hotmail.com>
2025-02-13 16:54:21 -0300

    LTP-7
    - Criando a página de criar conta

--- src/app/pages/criar-conta/criar-conta.component.ts
+++ src/app/pages/criar-conta/criar-conta.component.ts
@@ -1,11 +1,63 @@
-import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, Validators, FormsModule, ReactiveFormsModule, FormControl } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatButtonModule } from '@angular/material/button';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { MatIconModule } from '@angular/material/icon';
+import { merge } from 'rxjs';
 
 @Component({
-  selector: 'app-criar-conta',
-  imports: [],
+  selector: 'app-esqueceu-senha',
   templateUrl: './criar-conta.component.html',
-  styleUrl: './criar-conta.component.scss'
+  styleUrl: './criar-conta.component.scss',
+  imports: [
+    RouterModule,
+    MatButtonModule,
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule
+  ],
 })
 export class CriarContaComponent {
+  private _formBuilder = inject(FormBuilder);
+  readonly email = new FormControl('', [Validators.required, Validators.email]);
 
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+
+  isEditable = false;
+
+  emailError = signal('');
+
+  hide = signal(true);
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
+
+  constructor() {
+    merge(this.email.statusChanges, this.email.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.emailErrorMessage());
+  }
+
+  emailErrorMessage() {
+    if (this.email.hasError('required')) {
+      this.emailError.set('Por favor, digite um e-mail!');
+    } else if (this.email.hasError('email')) {
+      this.emailError.set('Por favor, digite um e-mail válido!');
+    } else {
+      this.emailError.set('');
+    }
+  }
 }
